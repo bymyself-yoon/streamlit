728c259cae3b9538e657097e5ab504aaffd33ee6
bymyself-yoon <120462702+bymyself-yoon@users.noreply.github.com>
2023-07-09 02:23:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def makeMarker(n):
     folium.Circle(
         location=top[i],
         radius = radius,
-        popup=df_top['구'].values.tolist()[i],
+        popup=<pre>+df_top['구'].values.tolist()[i]</pre>,
         color = color,
         fill = True,
         fill_color = color,
