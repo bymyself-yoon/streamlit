923a494f9fc164ec2b7bdbe4b67aa40a5cc0e651
bymyself-yoon <120462702+bymyself-yoon@users.noreply.github.com>
2023-07-09 16:08:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ def main():
 
       # extract sub-dataframe
       condition = (merged_gdf['sggnm'] == clicked_sggnm) & (merged_gdf['sidonm'] == clicked_sidonm )
-      filtered_df = merged_gdf[condition].iloc[:, 0:37].transpose()
+      filtered_df = merged_gdf[condition].iloc[:, 1:37].transpose()
       creation = filtered_df.iloc[[4, 0, 1, 2, 3]]
       # finance = filtered_df.iloc[5:11]
       # facilities = filtered_df.iloc[12:18]
