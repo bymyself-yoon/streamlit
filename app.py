e5f259c1d0dbbe57ed0d9926a75299e694861a8d
bymyself-yoon <120462702+bymyself-yoon@users.noreply.github.com>
2023-07-09 20:58:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ merged_seoul_df = pd.merge(seoul_df, admin_gdf, left_on='구', right_on='sggnm',
 merged_busan_df = pd.merge(busan_df, admin_gdf, left_on='구', right_on='sggnm', how='inner')
 
 # merged_df = pd.concat([merged_seoul_df, merged_busan_df])
-merged_df_all = pd.merge(busan_df, seoul_df, on = '구', how='outer')
+merged_df_all = pd.merge(merged_busan_df, merged_seoul_df, on = '구', how='outer')
 merged_gdf = gpd.GeoDataFrame(merged_df_all, geometry="geometry")
 
 st.write(merged_df_all)
