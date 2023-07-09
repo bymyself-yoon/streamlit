72eca0c8adf26ff48c745d4c5458c8e93aaff37d
bymyself-yoon <120462702+bymyself-yoon@users.noreply.github.com>
2023-07-09 18:10:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ def main():
       enjoyment = filtered_df.iloc[[26, 18, 19, 20, 21, 22, 23, 24, 25]]
       achievement = filtered_df.iloc[[34, 27, 28, 29, 30, 31, 32, 33]]
 
-      creation.style.applymap(draw_color_cell,color='#ff9090',subset=pd.IndexSlice[0, filtered_df.iloc[0]])
+      creation.style.applymap(color_cell,color='#ff9090',subset=pd.IndexSlice[0, filtered_df.iloc[0]])
       
       # print(filtered_df)
     
